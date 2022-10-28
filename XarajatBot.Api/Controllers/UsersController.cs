78fce00ad898b53f9a404fc27a3a75fbc463e564
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2022-10-28 23:52:20 +0500

    first step completed.

---
+++ XarajatBot.Api/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using XarajatBot.Api.Models;
+
+namespace XarajatBot.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public string GetUsers() => "All Users..";
+
+    [HttpGet("{id:int}")]
+    public UserModel GetUserById(int id) => new UserModel {Id = id, Name = "name1"};
+
+    [HttpGet("sorted")]
+    public string SortedUsers()
+    {
+        return "get sorted users..";
+    }
+
+    [HttpPost]
+    public string AddUser()
+    {
+        return "User added..";
+    }
+
+    [HttpPut("{id:int}")]
+    public string UpdateUser(int id)
+    {
+        return "User updated..";
+    }
+
+    [HttpDelete("{id:int}")]
+    public string DeleteUser(int id)
+    {
+        return "User deleted..";
+    }
+
+}
